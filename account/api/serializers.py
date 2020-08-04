fb242dc8856b18794266c0e9c81cf696fe0dd065
Sarathsathyan <sarathsathyan98@gmail.com>
2020-08-05 01:35:00 +0530

    changes in daash

--- account/api/serializers.py
+++ account/api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-
+from account.models import UserMore
 # User Serializer
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -14,6 +14,8 @@ class RegisterSerializer(serializers.ModelSerializer):
         fields = ('id','first_name','last_name', 'username', 'email', 'password')
         extra_kwargs = {'password': {'write_only': True}}
 
+
     def create(self, validated_data):
         user = User.objects.create_user(validated_data['username'],first_name=validated_data['first_name'],last_name=validated_data['last_name'], email=validated_data['email'], password=validated_data['password'])
+        print(user.id)
         return user
--- account/api/views.py
+++ account/api/views.py
@@ -3,20 +3,27 @@ from rest_framework.response import Response
 from knox.models import AuthToken
 from .serializers import UserSerializer, RegisterSerializer
 from django.shortcuts import render,redirect
+from django.contrib import messages
+from account.models import UserMore
 # Register API
 class RegisterAPI(generics.GenericAPIView):
     serializer_class = RegisterSerializer
+    def get(self,request):
 
+        return render(request,"accounts/register.html")
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
+
         context={
             "user": UserSerializer(user, context=self.get_serializer_context()).data,
             "token": AuthToken.objects.create(user)[1],
             "message":"successfully registered"
         }
-        return render(request,'accounts/register.html',context)
+        messages.success(request,"Register successfully")
+
+        return render(request,'accounts/login.html',context)
         # return Response({
         # "user": UserSerializer(user, context=self.get_serializer_context()).data,
         # "token": AuthToken.objects.create(user)[1]
@@ -29,10 +36,25 @@ from knox.views import LoginView as KnoxLoginView
 
 class LoginAPI(KnoxLoginView):
     permission_classes = (permissions.AllowAny,)
+    def get(self,request):
+
+        return render(request,"accounts/login.html")
 
     def post(self, request, format=None):
         serializer = AuthTokenSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.validated_data['user']
-        login(request, user)
+        try:
+
+            login(request, user)
+            if request.user.is_active:
+                context ={
+                    "first_name":user.first_name,
+                    "last_name":user.last_name,
+                    "email":user.email,
+                    "username":user.username
+                }
+                return render(request,"accounts/dash.html",context)
+        except:
+            print("error")
         return super(LoginAPI, self).post(request, format=None)
